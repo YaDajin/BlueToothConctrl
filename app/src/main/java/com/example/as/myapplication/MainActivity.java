7a65a9a626fe69557c98d59588ae4b99f976949c
YaDajin <13298721565@qq.com>
2018-12-26 11:30:49 +0800

    MainActivity

--- app/src/main/java/com/example/as/myapplication/MainActivity.java
+++ app/src/main/java/com/example/as/myapplication/MainActivity.java
@@ -1,13 +1,329 @@
 package com.example.as.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.MotionEvent;
+import android.view.View;
 
-    @Override
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.as.myapplication.BlueToothDeviceAdapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private BluetoothAdapter bluetoothAdapter;
+    private ListView listView;
+    private BlueToothDeviceAdapter adapter;
+    private TextView info =null;
+    private TextView text_state;
+    private TextView text_msg;
+
+    private final int BUFFER_SIZE = 1024;
+    private static final String NAME = "BT_DEMO";
+    private static final UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");//02001101-0001-1000-8080-00805F9BA9BA
+    private ConnectThread connectThread;
+    private ListenerThread listenerThread;
+    int x,y;//定义坐标系的x和y
+    @Override    //开始
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.info=(TextView)super.findViewById(R.id.info) ;
+        this.info.setOnTouchListener(new TouchListenerImp());
+        initView();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        initReceiver();
+        listenerThread = new ListenerThread();
+        listenerThread.start();
+    }
+    private class TouchListenerImp implements View.OnTouchListener {//触屏获取坐标事件
+        public boolean onTouch(View v,MotionEvent event){
+            MainActivity.this.info.setText("x="+event.getX()+"  y="+event.getY());
+            //x判断
+            if (event.getX()>130 && event.getX()<180){ x=0;}
+            else if (event.getX()>290 && event.getX()<340){x=1;}
+            else if (event.getX()>410 && event.getX()<470){x=2;}
+            else if (event.getX()>560 && event.getX()<650){x=3;}
+            else if (event.getX()>700 && event.getX()<790){x=4;}
+            else if (event.getX()>880 && event.getX()<950){x=5;}
+            else {x=-1;}
+            //y判断
+            if (event.getY()>900 && event.getY()<950){ y=0;}
+            else if (event.getY()>740 && event.getY()<800){y=1;}
+            else if (event.getY()>600 && event.getY()<670){y=2;}
+            else if (event.getY()>410 && event.getY()<480){y=3;}
+            else if (event.getY()>280 && event.getY()<340){y=4;}
+            else if (event.getY()>130 && event.getY()<190){y=5;}
+            else {y=-1;}
+            if (x!=-1&&y!=-1){
+                connectThread.sendMsg(String.valueOf(x)+String.valueOf(y)+"\r\n");//发送坐标数据
+            }
+            return false;
+        }
+    }
+    private void initView() {
+        findViewById(R.id.btn_openBT).setOnClickListener(this);
+        findViewById(R.id.btn_search).setOnClickListener(this);
+        findViewById(R.id.btn_send).setOnClickListener(this);
+        text_state = (TextView) findViewById(R.id.text_state);
+        text_msg = (TextView) findViewById(R.id.text_msg);
+
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new BlueToothDeviceAdapter(getApplicationContext(),R.layout.bluetooth_device_list_item);//bluetooth_device_list_item
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (bluetoothAdapter.isDiscovering()) {
+                    bluetoothAdapter.cancelDiscovery();
+                }
+                BluetoothDevice device = (BluetoothDevice) adapter.getItem(position);
+                connectDevice(device);//连接设备
+            }
+        });
+    }
+    private void initReceiver() {
+        //注册广播
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter);
+    }
+    /**
+     * 按键操作
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_openBT://“蓝牙控件”
+                openBlueTooth();
+                break;
+            case R.id.btn_search://打开“搜索控件”
+                searchDevices();
+                break;
+            case R.id.btn_send://“发送控件”
+                if (connectThread != null) {
+                    connectThread.sendMsg("OK"+"\r\n");//connectThread.sendMsg("A");//要发送的信息
+                }
+                break;
+        }
+    }
+    /**
+     * 开启蓝牙
+     */
+    private void openBlueTooth() {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "当前设备不支持蓝牙功能", Toast.LENGTH_SHORT).show();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+           /* Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(i);*/
+            bluetoothAdapter.enable();
+        }
+        //开启被其它蓝牙设备发现的功能
+        if (bluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            //设置为一直开启
+            i.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+            startActivity(i);
+        }
+    }
+    /**
+     * 搜索蓝牙设备
+     */
+    private void searchDevices() {
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        getBoundedDevices();
+        bluetoothAdapter.startDiscovery();
+    }
+    /**
+     * 获取已经配对过的设备
+     */
+    private void getBoundedDevices() {
+        //获取已经配对过的设备
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        //将其添加到设备列表中
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                adapter.add(device);
+            }
+        }
+    }
+    /**
+     * 连接蓝牙设备
+     */
+    private void connectDevice(BluetoothDevice device) {
+
+        text_state.setText(getResources().getString(R.string.connecting));
+
+        try {
+            //创建Socket
+            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(BT_UUID);
+            //启动连接线程
+            connectThread = new ConnectThread(socket, true);
+            connectThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //取消搜索
+        if (bluetoothAdapter != null && bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        //注销BroadcastReceiver，防止资源泄露
+        unregisterReceiver(mReceiver);
+    }
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //避免重复添加已经绑定过的设备
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    adapter.add(device);
+                    adapter.notifyDataSetChanged();
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Toast.makeText(MainActivity.this, "开始搜索", Toast.LENGTH_SHORT).show();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Toast.makeText(MainActivity.this, "搜索完毕", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    /**
+     * 连接线程
+     */
+    private class ConnectThread extends Thread {
+        private BluetoothSocket socket;
+        private boolean activeConnect;
+        InputStream inputStream;
+        OutputStream outputStream;
+        private ConnectThread(BluetoothSocket socket, boolean connect) {
+            this.socket = socket;
+            this.activeConnect = connect;
+        }
+        @Override
+        public void run() {
+            try {
+                //如果是自动连接 则调用连接方法
+                if (activeConnect) {
+                    socket.connect();
+                }
+                text_state.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        text_state.setText(getResources().getString(R.string.connect_success));
+                    }
+                });
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+                byte[] buffer = new byte[BUFFER_SIZE];//1024
+                int bytes;
+                while (true) {
+                    //读取数据
+                    bytes = inputStream.read(buffer);
+                    if (bytes > 0) {
+                        final byte[] data = new byte[bytes];
+                        System.arraycopy(buffer, 0, data, 0, bytes);
+                        text_msg.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                text_msg.setText(new String(data));
+                            }
+                        });
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                text_state.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        text_state.setText(getResources().getString(R.string.connect_error));
+                    }
+                });
+            }
+        }
+        /**
+         * 发送数据
+         */
+        public void sendMsg(final String msg) {
+
+            byte[] bytes = msg.getBytes();
+            if (outputStream != null) {
+                try {
+                    //发送数据
+                    outputStream.write(bytes);
+                    text_msg.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text_msg.setText(msg);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    text_msg.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text_msg.setText(getResources().getString(R.string.send_msg_error));
+                        }
+                    });
+                }
+            }
+        }
+    }
+    /**
+     * 监听线程
+     */
+    private class ListenerThread extends Thread {
+        private BluetoothServerSocket serverSocket;
+        private BluetoothSocket socket;
+        @Override
+        public void run() {
+            try {
+                serverSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, BT_UUID);
+                while (true) {
+                    //线程阻塞，等待别的设备连接
+                    socket = serverSocket.accept();
+                    text_state.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text_state.setText(getResources().getString(R.string.connecting));
+                        }
+                    });
+                    connectThread = new ConnectThread(socket, false);
+                    connectThread.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
